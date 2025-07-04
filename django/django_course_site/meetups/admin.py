d5fa8f2aa8e6b091bb377aa851cddeeedda9e248
gospodinovpetar <petarjordanov2003@gmail.com>
2025-07-04 15:47:49 +0300

    Adding participants, location, etc.

--- django/django_course_site/meetups/admin.py
+++ django/django_course_site/meetups/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from meetups.models import Meetup
+from meetups.models import Meetup, Location
 
 
 # Register your models here.
@@ -8,3 +8,8 @@ from meetups.models import Meetup
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'description')
     search_fields = ('name', 'slug', 'description')
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address')
+    search_fields = ('name', 'address')
