22d599fe5cefaf445dde42ca5d94ac3e6391a4b4
gospodinovpetar <petarjordanov2003@gmail.com>
2025-07-07 14:56:16 +0300

    Added a function to delete the uploaded images if the corresponding row is deleted

--- django/django_course_site/meetups/apps.py
+++ django/django_course_site/meetups/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class MeetupsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "meetups"
+
+    def ready(self):
+        # This line makes sure signals are registered
+        import meetups.signals
---
+++ django/django_course_site/meetups/signals.py
@@ -0,0 +1,14 @@
+import os
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from .models import Meetup
+
+@receiver(post_delete, sender=Meetup)
+def delete_meetup_image(sender, instance, **kwargs):
+    """
+    Deletes image file from filesystem
+    when corresponding `Meetup` object is deleted.
+    """
+    if instance.image and instance.image.path:
+        if os.path.isfile(instance.image.path):
+            os.remove(instance.image.path)
