b9f1f677b998077f03c2801e332073a1847b0dcb
gospodinovpetar <petarjordanov2003@gmail.com>
2025-07-15 13:04:32 +0300

    Adding authentication

--- fastapi/TodoApp/routers/auth.py
+++ fastapi/TodoApp/routers/auth.py
@@ -8,21 +8,13 @@ from sqlalchemy.orm import Session
 from database import SessionLocal
 from models import Users
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 router = APIRouter()
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-class CreateUserRequest(BaseModel):
-    username: str
-    email: str
-    first_name: str
-    last_name: str
-    password: str
-    role: str
 
 
 def get_db():
@@ -36,8 +28,31 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(db: Session, username: str, password: str):
+    user = (
+        db.query(Users).filter(Users.username == username).first() # We query the username
+    )
+    if not user:
+        return False
+    if not bcrypt_context.verify(
+        password, user.hashed_password # Automatically compares the hashed password
+    ):
+        return False
+    return True
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
 @router.post("/auth/", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+
     create_user_model = Users(
         username=create_user_request.username,
         email=create_user_request.email,
@@ -45,9 +60,20 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
         last_name=create_user_request.last_name,
         hashed_password=bcrypt_context.hash(create_user_request.password),
         role=create_user_request.role,
-        is_active = True
+        is_active=True,
     )
 
     db.add(create_user_model)
     db.commit()
 
+
+@router.post("/token/")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    user = authenticate_user(db, form_data.username, form_data.password)
+
+    if not user:
+        return "Failed Authentication"
+
+    return "Successfully Authenticated"
