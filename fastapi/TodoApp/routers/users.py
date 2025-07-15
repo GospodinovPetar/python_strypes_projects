22a00ef2ec4c54b0c92cfd5da798cf51139e1c81
gospodinovpetar <petarjordanov2003@gmail.com>
2025-07-15 16:09:43 +0300

    Added get user info (alone without the help of the course)

--- fastapi/TodoApp/routers/users.py
+++ fastapi/TodoApp/routers/users.py
@@ -2,12 +2,14 @@ from typing import Annotated
 from pydantic import BaseModel, Field
 from fastapi import FastAPI, Depends, HTTPException, Path, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy.testing.pickleable import User
+
 from models import *
 from database import SessionLocal
 from starlette import status
 from .auth import get_current_user
 
-router = APIRouter(prefix="/admin", tags=["admin"])
+router = APIRouter(prefix="/users", tags=["users"])
 
 
 def get_db():
@@ -21,3 +23,6 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
 
+@router.get("/")
+async def get_user(user_first_name : str, db: db_dependency):
+    return db.query(Users).filter(user_first_name == Users.first_name).first()
